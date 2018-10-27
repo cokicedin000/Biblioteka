c4f297a635f3d481044cd4521f3bb003acd33d3b
Edin Cokic <cokicedin000@gmail.com>
2018-10-28 00:14:34 +0200

    biblioteka

--- src/KreiranjeKnjige.java
+++ src/KreiranjeKnjige.java
@@ -1,4 +1,61 @@
+import java.sql.Date;
 
 public class KreiranjeKnjige {
+    private int brojKnjige;
+	private String imeKnjige;
+	private boolean status;
+	private Date datumPosudnjivanja;
 
+	KreiranjeKnjige() {
+
+	}
+
+	public KreiranjeKnjige(int brojKnjige, String imeKnjige) {
+		this.brojKnjige = brojKnjige;
+		this.imeKnjige = imeKnjige;
+
+	}
+
+	public KreiranjeKnjige(int brojKnjige, String imeKnjige, boolean status) {
+		this(brojKnjige, imeKnjige);
+		this.status = status;
+	}
+
+	public int getBrojKnjige() {
+		return brojKnjige;
+	}
+
+	public void setBrojKnjige(int brojKnjige) {
+		this.brojKnjige = brojKnjige;
+	}
+
+	public String getImeKnjige() {
+		return imeKnjige;
+	}
+
+	public void setImeKnjige(String imeKnjige) {
+		this.imeKnjige = imeKnjige;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public Date getDatumPosudnjivanja() {
+		return datumPosudnjivanja;
+	}
+
+	public void setDatumPosudnjivanja(java.util.Date datumPosudjivanja) {
+		this.datumPosudnjivanja = (Date) datumPosudjivanja;
+	}
+
+	@Override
+	public String toString() {
+		return "Knjiga [brojKnjige=" + brojKnjige + ", imeKnjige=" + imeKnjige + ", status=" + status
+				+ ", datumPosudnjivanja=" + datumPosudnjivanja + "]";
+	}
 }
--- src/KreiranjeRacuna.java
+++ src/KreiranjeRacuna.java
@@ -1,4 +1,45 @@
 
 public class KreiranjeRacuna {
+    
+    private int brojRacuna;
+	private String imeKorisnika;
+	private int brojKnjiga;
 
+	KreiranjeRacuna() {
+
+	}
+
+	public KreiranjeRacuna(int brojRacuna, String imeKorisnika) {
+		this.brojRacuna = brojRacuna;
+		this.imeKorisnika = imeKorisnika;
+	}
+
+	public KreiranjeRacuna(int brojRacuna, String imeKorisnika, int brojKnjiga) {
+		this(brojKnjiga, imeKorisnika);
+		this.brojKnjiga = brojKnjiga;
+	}
+
+	public int getBrojRacuna() {
+		return brojRacuna;
+	}
+
+	public void setBrojRacuna(int brojRacuna) {
+		this.brojRacuna = brojRacuna;
+	}
+
+	public String getIme() {
+		return imeKorisnika;
+	}
+
+	public void setIme(String ime) {
+		this.imeKorisnika = ime;
+	}
+
+	public int getBrojKnjiga() {
+		return brojKnjiga;
+	}
+
+	public void setBrojKnjiga(int brojKnjiga) {
+		this.brojKnjiga = brojKnjiga;
+	}
 }
